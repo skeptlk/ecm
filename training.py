cb69721b1be9c8e5aecc53785579bb856912b1d9
Bogdan <sceptlc@bk.ru>
2024-03-08 13:32:20 +0300

    Correction

--- training.py
+++ training.py
@@ -12,3 +12,19 @@ def get_metrics(y, y_pred, round_n=5):
       'mape': round(mape(y, y_pred), round_n),
     }
   )
+
+def correct(data):
+  datak = data.copy()
+  alpha = 0.5
+  theta = (data['tat'] + 273.15) / 288.15
+  theta_oat = (data['oat'] + 273.15) / 288.15
+  delta = data['p2e'] + 1013.25
+
+  datak['nfk'] = data['nf'] / (theta_oat ** alpha)
+  datak['n1ak'] = data['n1a'] / (theta_oat ** alpha)
+  datak['n1k'] = data['n1'] / (theta_oat ** alpha)
+  datak['n2ak'] = data['n2a'] / (theta_oat ** alpha)
+  datak['egtk'] = (data['egt'] + 273.15) / theta
+  datak['ffk'] = (data['ff']) / (delta * (theta_oat ** alpha))
+
+  return datak
