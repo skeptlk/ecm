c9a16ef6751f935eb07d4bcfd3b50eefd5a65201
Bogdan <sceptlc@bk.ru>
2024-03-27 13:25:45 +0300

    change chart

--- gui/streamlit.py
+++ gui/streamlit.py
@@ -10,6 +10,8 @@ def get_data():
   return pd.read_csv("https://drive.google.com/uc?export=view&id=1wCaZH0A-r6QRdMvAnwtKn8L1nUREWcn9", parse_dates=['reportts'])
 
 fleet = [
+  "VQ-BDU",
+  "VQ-BGU",
   # "RA73439",
   # "RA73440",
   # "RA73441",
@@ -18,48 +20,59 @@ fleet = [
   # "RA73444",
   # "RA73445",
   # "RA73446",
-  "VQ-BDU",
-  "VQ-BGU",
-  "VQ-BDI",
-  "VQ-BDV",
-  "VQ-BDW",
-  "VQ-BGR",
-  "VQ-BYI",
-  "VQ-BYJ"
+  # "VQ-BDI",
+  # "VQ-BDV",
+  # "VQ-BDW",
+  # "VQ-BGR",
+  # "VQ-BYI",
+  # "VQ-BYJ"
 ]
 
 col1, col2 = st.columns([2, 10])
 
-acnum = col1.selectbox("Select aircraft", fleet)
-pos = col1.radio("Select engine position", [1, 2])
+acnum = col1.selectbox("Aircraft", fleet)
+pos = col1.radio("Engine position", [1, 2])
 
 is_clicked = col1.button("Predict")
 
 df = get_data().query(f'acnum=="{acnum}" and pos=={pos}')
 
+
+leg = alt.Legend(
+    title="",
+    orient='none',
+    legendX=930,
+    legendY=30,
+)
+
 egtm = alt.Chart(df, height=700) \
   .mark_line(interpolate="basis") \
+  .transform_calculate(line="'EGTM'") \
   .encode(
     x=alt.X('reportts:T'),
-    y=alt.Y('egtm'),
+    y=alt.Y('egtm', title="EGT Margin"),
+    color=alt.Color('line:N', legend=leg)
   )
 
 delta = alt.Chart(df, height=700) \
+  .transform_calculate(line="'EGT Delta'") \
   .mark_point(filled=True) \
   .encode(
     x=alt.X('reportts:T'),
-    y=alt.Y('egt_delta'),
-    opacity=alt.value(0.4),
-    color=alt.ColorValue('orange')
-  ).interactive()
-
-delta_smooth = alt.Chart(df, height=700) \
-  .mark_line(interpolate="basis") \
-  .encode(
-    x=alt.X('reportts:T'),
-    y=alt.Y('egt_delta_smooth'),
-    color=alt.ColorValue('orange')
+    y=alt.Y('egt_delta', title=""),
+    opacity=alt.value(0.3),
+    color=alt.Color('line:N', legend=leg)
   )
 
+delta_smooth = alt.Chart(df, height=700) \
+  .transform_calculate(line="'EGT Delta Smooth'") \
+  .mark_line(interpolate="basis") \
+  .encode(
+    x=alt.X('reportts:T', title="Reported time"),
+    y=alt.Y('egt_delta_smooth', title=""),
+    color=alt.Color('line:N', legend=leg)
+  )
 
-col2.altair_chart(egtm + delta + delta_smooth, use_container_width=True)
+chart = egtm + delta + delta_smooth
+
+col2.altair_chart(chart, use_container_width=True)
