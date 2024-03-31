08428417e02f6d38220ec724b2884de5cc77048a
Bogdan <sceptlc@bk.ru>
2024-03-31 23:20:35 +0300

    early deltas

--- deltas.py
+++ deltas.py
@@ -1,6 +1,7 @@
 import pandas as pd 
 from sklearn.linear_model import LinearRegression
 from utils import * 
+from training import * 
 
 def train_engine_baseline(points: pd.DataFrame, x_param='n1ak', y_param='egtk'):
   model = LinearRegression()
@@ -13,7 +14,14 @@ def compute_egtm(points: pd.DataFrame, model: LinearRegression, x_param='n1ak'):
   return delta + offset
 
 
-def add_egt_delta_to_dataset(dataset: pd.DataFrame, bleed_param='prv', fleet=[], early=False):
+def add_egt_delta_to_dataset(
+    dataset: pd.DataFrame, 
+    bleed_param = 'prv', 
+    fleet = [], 
+    early = False, 
+    interval = pd.to_timedelta('30D'),
+    acms_data: pd.DataFrame = None
+):
   for acnum in fleet: 
     for pos in [1, 2]:
       for bleed in [0, 1]:
@@ -23,11 +31,18 @@ def add_egt_delta_to_dataset(dataset: pd.DataFrame, bleed_param='prv', fleet=[],
         if subset.shape[0] == 0:
           continue
 
-        early_start = subset.iloc[0]['reportts'] + pd.to_timedelta('0')
-        early_end = subset.iloc[0]['reportts'] + pd.to_timedelta('30D')
-        early_filter = (subset['reportts'] >= early_start) & (subset['reportts'] <= early_end) 
+        if acms_data is None: 
+          early_end = subset.iloc[0]['reportts'] + interval
+          early_filter = subset['reportts'] <= early_end
+          baseline = train_engine_baseline(subset[early_filter] if early else subset)
+        else:
+          acms_index = (acms_data['pos'] == pos) & (acms_data['acnum'] == acnum) & (acms_data[bleed_param] == bleed)
+          acms_subset = acms_data[acms_index]
+          early_end = acms_subset.iloc[0]['reportts'] + interval
+          acms_early = acms_subset[acms_subset['reportts'] <= early_end]
+          print(acms_subset.iloc[0]['reportts'], acms_early.shape)
+          baseline = train_engine_baseline(correct(acms_early))
 
-        baseline = train_engine_baseline(subset[early_filter] if early else subset)
         egt_delta = compute_egtm(subset, baseline)
         dataset.loc[subset_index, 'egt_delta'] = egt_delta
 
