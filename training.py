5fcb32b665834e006339fe3ca907012c5d53d5b7
skeptlk <sceptlc@bk.ru>
2024-04-04 10:02:25 +0300

    Delta errors analysis

--- training.py
+++ training.py
@@ -1,12 +1,13 @@
-from sklearn.metrics import mean_squared_error as mse, \
+from sklearn.metrics import \
   mean_absolute_error as mae, \
   mean_absolute_percentage_error as mape, \
+  root_mean_squared_error as rmse, \
   r2_score as r2
 
 def get_metrics(y, y_pred, round_n=5):
   return (
     {
-      'rmse': round(mse(y, y_pred, squared=False), round_n),
+      'rmse': round(rmse(y, y_pred), round_n),
       'mae': round(mae(y, y_pred), round_n),
       'r2': round(r2(y, y_pred), round_n),
       'mape': round(mape(y, y_pred), round_n),
